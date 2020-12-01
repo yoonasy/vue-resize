5d89e8e0e66ddb053adeebad28d5ce6f82975243
js52 <js52@example.com>
2020-12-02 03:42:17 +0400

    fix

--- docs-src/vue.config.js
+++ docs-src/vue.config.js
@@ -1,5 +1,9 @@
 const path = require('path')
 
+function resolve (src) {
+  return path.join(__dirname, src)
+}
+
 module.exports = {
   publicPath: '/vue3-resize/',
   outputDir: '../docs',
@@ -7,5 +11,8 @@ module.exports = {
     historyApiFallback: true,
     noInfo: true,
     contentBase: path.join(__dirname, '..', 'docs'),
+  },
+  chainWebpack: config => {
+    config.resolve.alias.set('vue$', resolve('../node_modules/vue'))
   },
 }
